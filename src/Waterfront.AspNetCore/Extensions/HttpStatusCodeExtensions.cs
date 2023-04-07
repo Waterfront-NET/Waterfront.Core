ffc64945d96eaf4618d1534aea93f6cb333dd9ae
2CHEVSKII <prsroman3@gmail.com>
2023-04-07 15:03:54 +0300

    Exported Waterfront.AspNetCore into separate project

--- src/Waterfront.AspNetCore/Extensions/HttpStatusCodeExtensions.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Net;
-
-namespace Waterfront.AspNetCore.Extensions;
-
-public static class HttpStatusCodeExtensions
-{
-    public static int ToInt32(this HttpStatusCode self) => (int)self;
-}
