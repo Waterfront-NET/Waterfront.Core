d85faa98feac78f82cbd6cbe0e217064fde4e94b
2CHEVSKII <prsroman3@gmail.com>
2023-04-07 12:41:03 +0300

    Add BasicAuthParser

--- src/Waterfront.AspNetCore/Configuration/Endpoints/EndpointOptions.cs
+++ src/Waterfront.AspNetCore/Configuration/Endpoints/EndpointOptions.cs
@@ -5,6 +5,6 @@ namespace Waterfront.AspNetCore.Configuration.Endpoints;
 public class WaterfrontEndpointOptions
 {
     public PathString TokenEndpoint { get; set; } = new PathString("/token");
-    public PathString InfoEndpoint { get; set; }
-    public PathString PublicKeyEndpoint { get; set; }
+    public PathString InfoEndpoint { get; set; } = new PathString("/info");
+    public PathString PublicKeyEndpoint { get; set; } = new PathString("/public-key");
 }
---
+++ src/Waterfront.Core/Utility/Parsing/BasicAuthParser.cs
@@ -0,0 +1,48 @@
+﻿namespace Waterfront.Core.Utility.Parsing;
+
+public static class BasicAuthParser
+{
+    public static (string username, string password) ParseAuthString(string? input)
+    {
+        if ( string.IsNullOrEmpty(input) )
+        {
+            return (string.Empty, string.Empty);
+        }
+
+        if ( !input.Contains(':') )
+        {
+            return (input, string.Empty);
+        }
+
+        var delimiterIndex = input.IndexOf(':');
+
+        var username = input.Substring(0, delimiterIndex);
+
+        if ( delimiterIndex == input.Length - 1 )
+        {
+            return (username, string.Empty);
+        }
+
+        var password = input.Substring(delimiterIndex + 1);
+
+        return (username, password);
+    }
+
+    public static bool IsBasicAuth(string? headerValue)
+    {
+        if ( string.IsNullOrEmpty(headerValue) )
+            return false;
+
+        return headerValue.StartsWith("Basic ");
+    }
+
+    public static (string username, string password) ParseHeaderValue(string? input)
+    {
+        if ( !IsBasicAuth(input) )
+        {
+            return (string.Empty, string.Empty);
+        }
+
+        return ParseAuthString(input!.Substring(6));
+    }
+}
