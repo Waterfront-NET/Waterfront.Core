5c77105536a2bb42b73cd6ffcb4dfeb6a3b3ce0a
2CHEVSKII <prsroman3@gmail.com>
2023-05-20 16:15:54 +0300

    Init TokenRequestScopeCollection

---
+++ src/Waterfront.Common/Tokens/Requests/Collections/TokenRequestScopeCollection.cs
@@ -0,0 +1,10 @@
+﻿namespace Waterfront.Common.Tokens.Requests.Collections;
+
+/// <summary>
+/// Provides a collection to hold instances of <see cref="TokenRequestScope"/>
+/// and convenient methods to manage authorized/forbidden scopes
+/// </summary>
+public class TokenRequestScopeCollection
+{
+    // TODO
+}
