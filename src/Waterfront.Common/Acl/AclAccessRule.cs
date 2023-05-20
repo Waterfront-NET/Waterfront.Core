44d8c03e04631924eb5fda4a019151652da59aa0
2CHEVSKII <prsroman3@gmail.com>
2023-05-20 21:54:50 +0300

    Add doc comments to models

--- src/Waterfront.Common/Acl/AclAccessRule.cs
+++ src/Waterfront.Common/Acl/AclAccessRule.cs
@@ -2,9 +2,21 @@
 
 #pragma warning disable CS8618
 
+/// <summary>
+/// Represents a resource access rule
+/// </summary>
 public class AclAccessRule
 {
+    /// <summary>
+    /// The resource type
+    /// </summary>
     public AclResourceType Type { get; init; }
+    /// <summary>
+    /// Name of the resource, may include glob pattern characters
+    /// </summary>
     public string Name { get; init; }
+    /// <summary>
+    /// Defines a set of actions user is allowed to perform on a resource
+    /// </summary>
     public IEnumerable<AclResourceAction> Actions { get; init; }
 }
