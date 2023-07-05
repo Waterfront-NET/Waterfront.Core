323715001ecf0148b54cd1d35706895e7dfb2c85
2CHEVSKII <prsroman3@gmail.com>
2023-07-05 23:37:05 +0300

    Implement basic version of AclAuthenticationService

---
+++ src/Waterfront.Core/Authentication/AclAuthenticationService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Logging;
+using Waterfront.Common.Authentication;
+using Waterfront.Common.Tokens.Requests;
+
+namespace Waterfront.Core.Authentication;
+
+public class AclAuthenticationService : IAclAuthenticationService
+{
+    protected ILogger<AclAuthenticationService> Logger { get; }
+    protected IAclAuthenticationSchemeProvider AuthenticationSchemeProvider { get; }
+    protected IAclAuthenticationHandlerProvider AuthenticationHandlerProvider { get; }
+
+    public AclAuthenticationService(
+        ILogger<AclAuthenticationService> logger,
+        IAclAuthenticationSchemeProvider authenticationSchemeProvider,
+        IAclAuthenticationHandlerProvider handlerProvider
+    )
+    {
+        Logger = logger;
+        AuthenticationSchemeProvider = authenticationSchemeProvider;
+        AuthenticationHandlerProvider = handlerProvider;
+    }
+
+    public async ValueTask<AclAuthenticationResult> AuthenticateAsync(TokenRequest request)
+    {
+        IEnumerable<AclAuthenticationScheme> availableSchemes =
+            await AuthenticationSchemeProvider.GetSchemesForRequestAsync(request);
+
+        foreach (AclAuthenticationScheme scheme in availableSchemes)
+        {
+            IAclAuthenticationHandler handler = await AuthenticationHandlerProvider.GetHandlerAsync(scheme);
+            await handler.InitializeAsync(scheme);
+
+            AclAuthenticationResult result = await handler.AuthenticateAsync(request);
+
+            if (result.IsSuccessful)
+            {
+                return result;
+            }
+        }
+
+        return AclAuthenticationResult.Fail(request.Id);
+    }
+}
